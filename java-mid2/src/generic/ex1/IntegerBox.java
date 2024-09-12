877f944c4e36f4485df34b6d3646f13ae64bc058
cylcoder <cylcoder@gmail.com>
2024-09-12 16:46:04 +0900

    Section 2: 제네릭이 필요한 이유

---
+++ java-mid2/src/generic/ex1/IntegerBox.java
@@ -0,0 +1,15 @@
+package generic.ex1;
+
+public class IntegerBox {
+
+    private Integer value;
+
+    public Integer get() {
+        return value;
+    }
+
+    public void set(Integer value) {
+        this.value = value;
+    }
+
+}
