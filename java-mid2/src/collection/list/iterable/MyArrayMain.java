5833f93cfa1eec219642701bf4ae4fe464a5fd86
cylcoder <cylcoder@gmail.com>
2024-09-24 15:15:11 +0900

    Section 11: 순회2 - 향상된 for문

--- java-mid2/src/collection/list/iterable/MyArrayMain.java
+++ java-mid2/src/collection/list/iterable/MyArrayMain.java
@@ -7,8 +7,16 @@ public class MyArrayMain {
     public static void main(String[] args) {
         MyArray myArray = new MyArray(new int[]{1, 2, 3, 4});
         Iterator<Integer> iterator = myArray.iterator();
+        System.out.println("iterator 사용");
+        
         while (iterator.hasNext()) {
-            System.out.println(iterator.next());
+            System.out.println("value = " + iterator.next());
+        }
+        
+        // 추가
+        System.out.println("for-each 사용");
+        for (int value : myArray) {
+            System.out.println("value = " + value);
         }
     }
 
